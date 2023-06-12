65727d8011bc3d5ea657b45461b67dd8e3ac8a19
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2023-06-12 10:04:51 -0400

    Base funcionando con endpoints para crear user y login con entrega de token

---
+++ src/app.py
@@ -0,0 +1,137 @@
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+import os
+from flask import Flask, request, jsonify, make_response
+from flask_migrate import Migrate
+from flask_swagger import swagger
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
+from admin import setup_admin
+from models import db, User, Pet, Address
+from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
+import jwt 
+import datetime
+from functools import wraps
+
+# from models import Person
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+db_key = os.getenv("SECRET_KEY")
+db_url = os.getenv("DATABASE_URL")
+
+app.config["SECRET_KEY"] = db_key
+
+if db_url is not None:
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace(
+        "postgres://", "postgresql://"
+    )
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+    
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+MIGRATE = Migrate(app, db)
+db.init_app(app)
+CORS(app)
+setup_admin(app)
+
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+
+        if "x-access-tokens" in request.headers:
+            token = request.headers["x-access-tokens"]
+
+        if not token:
+            return jsonify({"message": "a valid token is missing"})
+
+        try:
+            data = jwt.decode(token, app.config["SECRET_KEY"])
+            current_user = User.query.filter_by(public_id=data["public_id"]).first()
+        except:
+            return jsonify({"message": "token is invalid"})
+
+        return f(current_user, *args, **kwargs)
+    
+    return decorator
+
+
+# Handle/serialize errors like a JSON object
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
+
+# generate sitemap with all your endpoints
+@app.route("/")
+def sitemap():
+    return generate_sitemap(app)
+
+
+@app.route("/user", methods=["GET"])
+def handle_hello():
+    response_body = {"msg": "Hello, this is your GET /user response "}
+
+    return jsonify(response_body), 200
+
+
+@app.route("/register", methods=["GET", "POST"])
+def signup_user():
+    data = request.get_json()
+
+    hashed_password = generate_password_hash(data["password"], method="sha256")
+
+    new_user = User(
+        public_id=uuid.uuid4(),
+        user_name=data["user_name"],
+        email=data["email"],
+        password=hashed_password,
+        first_name=data["first_name"],
+        last_name=data["last_name"],
+        donor=data["donor"],
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "registered successfully"})
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login_user():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response(
+            "could not verify",
+            401,
+            {"WWW.Authentication": 'Basic realm: "login required"'},
+        )
+
+    user = User.query.filter_by(user_name=auth.username).first()
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode(
+            {
+                "public_id": user.public_id,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+            },
+            app.config["SECRET_KEY"],
+        )
+        return jsonify({"token": token})
+
+    return make_response(
+        "could not verify", 401, {"WWW.Authentication": 'Basic realm: "login required"'}
+    )
+
+
+# this only runs if `$ python src/app.py` is executed
+if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=PORT, debug=False)
---
+++ src/models.py
@@ -0,0 +1,119 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+owners_pets = db.Table(
+    "owners_pets",
+    db.Column("user_id", db.ForeignKey("users.id")),
+    db.Column("pet_id", db.ForeignKey("pets.id")),
+)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True, nullable=False)
+    user_name = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), unique=False, nullable=False)
+    first_name = db.Column(db.String(50), unique=False, nullable=False)
+    last_name = db.Column(db.String(50), unique=False, nullable=False)
+    donor = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    owner = db.relationship("Pet", secondary=owners_pets, back_populates="pet")
+    house = db.relationship("Address", back_populates="home_owner")
+
+    def __init__(
+        self, public_id, user_name, email, password, first_name, last_name, donor
+    ):
+        self.public_id = public_id
+        self.user_name = user_name
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.donor = donor
+
+    def __repr__(self):
+        return f'User("{self.user_name}")'
+
+    def serialize(self):
+        return {
+            "Usuario": self.user_name,
+            "Email": self.email,
+            "Dador": self.giver,
+        }
+
+
+class Pet(db.Model):
+    __tablename__ = "pets"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=False, nullable=True)
+    specie = db.Column(db.Integer, unique=False, nullable=True)
+    age = db.Column(db.Integer, unique=False, nullable=True)
+    size = db.Column(db.Integer, unique=False, nullable=True)
+    need_backyard = db.Column(db.Boolean, unique=False, nullable=True)
+    id_origin = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+    id_owner = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+
+    pet = db.relationship("User", secondary=owners_pets, back_populates="owner")
+
+    def __init__(self, name, specie, size, need_backyard, id_origin, id_actual):
+        self.name = name
+        self.specie = specie
+        self.size = size
+        self.need_backyard = need_backyard
+        self.id_origin = id_origin
+        self.id_actual = id_actual
+
+    def __repr__(self):
+        return f'Pet("{self.name}","{self.specie})'
+
+    def serialize(self):
+        return {
+            "Nombre": self.name,
+            "Especie": self.specie,
+            "Tamaño": self.size,
+            "Necesita Patio": self.need_backyard,
+            "Entregado por": self.id_origin,
+        }
+
+
+class Address(db.Model):
+    __tablename__ = "addresses"
+    id = db.Column(db.Integer, primary_key=True)
+    street = db.Column(db.String(30), unique=False, nullable=False)
+    building_number = db.Column(db.Integer, unique=False, nullable=False)
+    department_number = db.Column(db.Integer, unique=False, nullable=False)
+    commune = db.Column(db.Integer, unique=False, nullable=False)
+    region = db.Column(db.Integer, unique=False, nullable=True)
+    has_backyard = db.Column(db.Boolean, unique=False, nullable=True)
+    habitant = db.Column(
+        db.Integer, db.ForeignKey("users.id"), unique=True, nullable=False
+    )
+
+    home_owner = db.relationship("User", back_populates="house")
+
+    def __init__(
+        self, street, building_number, department_number, commune, region, has_backyard
+    ):
+        self.street = street
+        self.building_number = building_number
+        self.department_number = department_number
+        self.commune = commune
+        self.region = region
+        self.has_backyard = has_backyard
+
+    def __rep__(self):
+        return f'Address("{self.street}","{self.building_number}","{self.commune}")'
+
+    def serialize(self):
+        return {
+            "Calle": self.street,
+            "Numero": self.building_number,
+            "Departamento": self.department_number,
+            "Comuna": self.commune,
+            "Región": self.region,
+            "Tiene Patio": self.has_backyard,
+        }
